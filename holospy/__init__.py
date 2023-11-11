1d1af89f5adec0b7124dccc119316fb8661f3452
Eric Prestat <eric.prestat@gmail.com>
2023-11-11 07:32:43 +0000

    Use `setuptools_scm` library to set version at build time and runtime for editable install

--- holospy/__init__.py
+++ holospy/__init__.py
@@ -1,4 +1,37 @@
-from . import data, release_info, signals, reconstruct, tools
+# -*- coding: utf-8 -*-
+# Copyright 2007-2023 The HyperSpy developers
+#
+# This file is part of HyperSpy.
+#
+# HyperSpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HyperSpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HyperSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
+
+from pathlib import Path
+
+from . import data, signals, reconstruct, tools
+
+
+if Path(__file__).parent.parent.name == "site-packages":  # pragma: no cover
+    # Tested in the "Package & Test" workflow on GitHub CI
+    from importlib.metadata import version
+
+    __version__ = version("rosettasciio")
+else:
+    # Editable install
+    from setuptools_scm import get_version
+
+    __version__ = get_version(Path(__file__).parent.parent)
+
 
 __all__ = [
     "__version__",
@@ -11,10 +44,3 @@ __all__ = [
 
 def __dir__():
     return sorted(__all__)
-
-
-__version__ = release_info.version
-__author__ = release_info.author
-__copyright__ = release_info.copyright
-__license__ = release_info.license
-__status__ = release_info.status
--- holospy/release_info.py
+++
@@ -1,6 +0,0 @@
-name = "holospy"
-version = "0.1.dev0"
-author = "Holospy Developers"
-copyright = "Copyright 2016-2023, The HoloSpy developers"
-license = "GPLv3+"
-status = "development"
