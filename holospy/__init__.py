ef645c783bd62e3d09029dbb4f04296527d427a4
Eric Prestat <eric.prestat@gmail.com>
2023-12-01 17:30:28 +0100

    Improve getting version when installing from git archive

--- holospy/__init__.py
+++ holospy/__init__.py
@@ -16,21 +16,35 @@
 # You should have received a copy of the GNU General Public License
 # along with HyperSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
 
+from importlib.metadata import version
 from pathlib import Path
 
 from . import data, signals, reconstruct, tools
 
 
-if Path(__file__).parent.parent.name == "site-packages":  # pragma: no cover
-    # Tested in the "Package & Test" workflow on GitHub CI
-    from importlib.metadata import version
+__version__ = version("holospy")
 
-    __version__ = version("holospy")
-else:
-    # Editable install
-    from setuptools_scm import get_version
+# For development version, `setuptools_scm` will be used at build time
+# to get the dev version, in case of missing vcs information (git archive,
+# shallow repository), the fallback version defined in pyproject.toml will
+# be used
 
-    __version__ = get_version(Path(__file__).parent.parent)
+# If we have an editable installed from a git repository try to use
+# `setuptools_scm` to find a more accurate version:
+# `importlib.metadata` will provide the version at installation
+# time and for editable version this may be different
+
+# we only do that if we have enough git history, e.g. not shallow checkout
+_root = Path(__file__).resolve().parents[1]
+if (_root / ".git").exists() and not (_root / ".git/shallow").exists():
+    try:
+        # setuptools_scm may not be installed
+        from setuptools_scm import get_version
+
+        __version__ = get_version(_root)
+    except ImportError:  # pragma: no cover
+        # setuptools_scm not install, we keep the existing __version__
+        pass
 
 
 __all__ = [
